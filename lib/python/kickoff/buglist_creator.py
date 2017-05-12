5e704d1a349a34e2eb56f052fa7ba3b9a9c164cf
Connor Sheehan <csheehan@mozilla.com>
2017-05-12 15:57:50 -0400

    Add more tests, put Bugzilla link through shortener, test Hg JSON API as it is unstable

--- lib/python/kickoff/buglist_creator.py
+++ lib/python/kickoff/buglist_creator.py
@@ -5,14 +5,15 @@ from operator import itemgetter
 from pkg_resources import parse_version
 from simplejson import JSONDecodeError
 
-BACKOUT_REGEX = r'backout|back out|backed out|backing out'
-BUGZILLA_BUGLIST_TEMPLATE = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id={bugs}\n'
-BUG_NUMBER_REGEX = r'bug \d*'
+BACKOUT_REGEX = r'back(\s?)out|backed out|backing out'
+BUGZILLA_BUGLIST_TEMPLATE = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id={bugs}'
+BUG_NUMBER_REGEX = r'bug \d+'
 CHANGELOG_TO_FROM_STRING = '{product}_{version}_RELEASE'
 CHANGESET_URL_TEMPLATE = 'https://hg.mozilla.org/{release_branch}/json-pushes?fromchange={from_version}&tochange={to_version}&full=1'
 LIST_DESCRIPTION_TEMPLATE = 'Comparing Mercurial tag {from_version} to {to_version}:\n'
 MERCURIAL_TAGS_URL_TEMPLATE = 'https://hg.mozilla.org/{release_branch}/json-tags'
 NO_BUGS = ''  # Return this when bug list can't be created
+URL_SHORTENER_TEMPLATE = 'https://bugzilla.mozilla.org/rest/bitly/shorten?url={url}'
 
 log = logging.getLogger(__name__)
 
@@ -26,8 +27,8 @@ def create_bugs_url(release):
     """
     Creates list of bugs and backout bugs for release-drivers email
 
-    :param release: Dict containing information about release, from Ship-It
-    :return: Tuple: (Text + link to standard bugs in changeset, text + link to backout bugs in changeset,)
+    :param release: dict -> containing information about release, from Ship-It
+    :return: str -> description of compared releases, with Bugzilla links containing all bugs in changeset
     """
     try:
         current_version_dot = release['version']
@@ -38,80 +39,87 @@ def create_bugs_url(release):
         product = release['product']
         branch = release['branch']
 
-        current_version_tag, previous_version_tag = _get_tag_versions(product, branch, current_version_dot)
+        current_version_tag, previous_version_tag = get_tag_versions(product, branch, current_version_dot)
         description_string = LIST_DESCRIPTION_TEMPLATE.format(from_version=previous_version_tag,
                                                               to_version=current_version_tag)
 
         resp = requests.get(CHANGESET_URL_TEMPLATE.format(release_branch=branch,
-                                                          from_version=previous_version_tag, to_version=current_version_tag))
+                                                          from_version=previous_version_tag,
+                                                          to_version=current_version_tag))
         changeset_data = resp.json()
 
-        unique_bugs, unique_backout_bugs = set(), set()
-        for data in changeset_data.values():
-            for changeset in data['changesets']:
-                if _is_test_only_change(changeset):
-                    continue
-
-                changeset_desc_lower = changeset['desc'].lower()
-                bug_re = re.search(BUG_NUMBER_REGEX, changeset_desc_lower)
-
-                if bug_re:
-                    bug_number = bug_re.group().split(' ')[1]
-
-                    if _is_backout_bug(changeset_desc_lower):
-                        unique_backout_bugs.add(bug_number)
-                    else:
-                        unique_bugs.add(bug_number)
+        unique_bugs, unique_backout_bugs = get_bugs_in_changeset(changeset_data)
 
         if unique_bugs or unique_backout_bugs:
-            return _format_return_value(description_string, unique_bugs, unique_backout_bugs)
+            return format_return_value(description_string, unique_bugs, unique_backout_bugs)
         else:
-            return ''
+            return NO_BUGS
 
     except (requests.HTTPError, JSONDecodeError, BuglistCreatorException,) as err:
         log.info(err)
         return NO_BUGS
 
 
-def _is_test_only_change(changeset):
+def get_bugs_in_changeset(changeset_data):
+    unique_bugs, unique_backout_bugs = set(), set()
+    for data in changeset_data.values():
+        for changeset in data['changesets']:
+            if is_test_only_change(changeset):
+                continue
+
+            changeset_desc_lower = changeset['desc'].lower()
+            bug_re = re.search(BUG_NUMBER_REGEX, changeset_desc_lower)
+
+            if bug_re:
+                bug_number = bug_re.group().split(' ')[1]
+                print bug_re.group()
+
+                if is_backout_bug(changeset_desc_lower):
+                    unique_backout_bugs.add(bug_number)
+                else:
+                    unique_bugs.add(bug_number)
+
+    return unique_bugs, unique_backout_bugs
+
+
+def is_test_only_change(changeset):
     return 'a=test-only' in changeset['desc']
 
 
-def _is_backout_bug(changeset_description_lowercase):
+def is_backout_bug(changeset_description_lowercase):
     return re.search(BACKOUT_REGEX, changeset_description_lowercase)
 
 
-def _create_buglist_url(buglist, backout_buglist):
+def create_short_url_with_prefix(buglist, backout_buglist):
     # Create link if there are bugs, else empty string
-    if buglist:
-        regular_bugs = '%2C'.join(buglist)
-        regular_bugs = 'Relevant bugs: ' + BUGZILLA_BUGLIST_TEMPLATE.format(bugs=regular_bugs)
-    else:
-        regular_bugs = ''
+    urls = []
+    for set_of_bugs, prefix in ((buglist, 'Bugs since previous changeset: ',),
+                               (backout_buglist, 'Backouts since previous changeset: ',)):
+        if set_of_bugs:
+            long_bugzilla_link = BUGZILLA_BUGLIST_TEMPLATE.format(bugs='%2C'.join(set_of_bugs))
+            url = requests.get(URL_SHORTENER_TEMPLATE.format(url=long_bugzilla_link)).json()['url']
+            url = prefix + url + '\n'
+        else:
+            url = ''
 
-    # Do the same thing for backouts
-    if backout_buglist:
-        backout_bugs = '%2C'.join(backout_buglist)
-        backout_bugs = 'Backout bugs: ' + BUGZILLA_BUGLIST_TEMPLATE.format(bugs=backout_bugs)
-    else:
-        backout_bugs = ''
+        urls.append(url)
 
-    return regular_bugs, backout_bugs
+    return urls[0], urls[1]
 
 
-def _dot_version_to_tag_version(product, dot_version):
+def dot_version_to_tag_version(product, dot_version):
     underscore_version = dot_version.replace('.', '_')
     return CHANGELOG_TO_FROM_STRING.format(product=product.upper(), version=underscore_version)
 
 
-def _tag_version_to_dot_version_parse(tag):
+def tag_version_to_dot_version_parse(tag):
     dot_version = '.'.join(tag.split('_')[1:-1])
     return parse_version(dot_version)
 
 
-def _get_tag_versions(product, branch, current_version_dot):
+def get_tag_versions(product, branch, current_version_dot):
     """Gets the previous hg version tag for the product and branch, given the current version tag"""
-    current_version_tag = _dot_version_to_tag_version(product, current_version_dot)
+    current_version_tag = dot_version_to_tag_version(product, current_version_dot)
 
     tag_url = MERCURIAL_TAGS_URL_TEMPLATE.format(release_branch=branch)
     response_json = requests.get(tag_url).json()
@@ -119,7 +127,8 @@ def _get_tag_versions(product, branch, current_version_dot):
     def _invalid_tag_filter(tag):
         """Filters by product and removes incorrect major version + base, end releases"""
         major_version = current_version_dot.split('.')[0]
-        prod_major_version_re = r'^{product}_{major_version}'.format(product=product.upper(), major_version=major_version)
+        prod_major_version_re = r'^{product}_{major_version}'.format(product=product.upper(),
+                                                                     major_version=major_version)
 
         return 'BASE' not in tag and \
                'END' not in tag and \
@@ -129,33 +138,42 @@ def _get_tag_versions(product, branch, current_version_dot):
     # Get rid of irrelevant tags, sort by date and extract the tag string
     tags = set(map(itemgetter('tag'), response_json['tags']))
     tags = filter(_invalid_tag_filter, tags)
-    dot_tag_version_mapping = zip(map(_tag_version_to_dot_version_parse, tags), tags)
+    dot_tag_version_mapping = zip(map(tag_version_to_dot_version_parse, tags), tags)
     dot_tag_version_mapping = sorted(dot_tag_version_mapping, key=itemgetter(0))
 
     try:
         next_version_index = map(itemgetter(0), dot_tag_version_mapping).index(parse_version(current_version_dot)) - 1
     except ValueError as err:
-        raise BuglistCreatorException("Couldn't find a tag for {}".format(current_version_tag))
+        raise BuglistCreatorException("Couldn't find a tag for {}: {}".format(current_version_tag, err))
 
     return current_version_tag, dot_tag_version_mapping[next_version_index][1]
 
 
-def _format_return_value(description, unique_bugs, unique_backout_bugs):
-    reg_bugs_link, backout_bugs_link = _create_buglist_url(unique_bugs, unique_backout_bugs)
-    return """{description}{regular_bz_url}{backout_bz_url}""".format(description=description,
-                                                                      regular_bz_url=reg_bugs_link,
-                                                                      backout_bz_url=backout_bugs_link)
+def format_return_value(description, unique_bugs, unique_backout_bugs):
+    reg_bugs_link, backout_bugs_link = create_short_url_with_prefix(unique_bugs, unique_backout_bugs)
+    return_str = '{description}{regular_bz_url}{backout_bz_url}'.format(description=description,
+                                                                        regular_bz_url=reg_bugs_link,
+                                                                        backout_bz_url=backout_bugs_link)
 
+    return return_str[:-1] if return_str.endswith('\n') else return_str  # Remove trailing newline
 
-if __name__ == '__main__':
-    import pickle
-    dl = pickle.load(open('d.p', 'r'))
-
-    dum = []
-    for d in dl:
-        if d['product'] == 'fennec':
-            s = create_bugs_url(d)
-            dum.append(s)
-
-    from pprint import pprint
-    pprint(dum)
+#
+# if __name__ == '__main__':
+#     # import pickle
+#     # dl = pickle.load(open('d.p', 'r'))
+#     #
+#     # dum = []
+#     # for d in dl:
+#     #     if d['product'] == 'fennec':
+#     #         s = create_bugs_url(d)
+#     #         dum.append(s)
+#     #
+#     # from pprint import pprint
+#     # pprint(dum)
+#
+#     url = 'https://hg.mozilla.org/releases/mozilla-release/json-pushes?fromchange=d345b657d381&tochange=f87a819106bd&full=1'
+#     changeset_data = requests.get(url).json()
+#
+#     bugs, backouts = get_bugs_in_changeset(changeset_data)
+#     print bugs
+#     print backouts
--- lib/python/kickoff/test/test_buglist_creator.py
+++ lib/python/kickoff/test/test_buglist_creator.py
@@ -1,101 +1,92 @@
+import requests
 import unittest
-from kickoff.buglist_creator import create_bugs_url
+from kickoff.buglist_creator import is_test_only_change, create_bugs_url, is_backout_bug, get_tag_versions, \
+    format_return_value, create_short_url_with_prefix, get_bugs_in_changeset
 
 
-class TestCreateBugsUrlDesktop(unittest.TestCase):
+class TestBuglistCreator(unittest.TestCase):
 
-    def setUp(self):
-        self.release_object_51_0_1 = {
-            'branch': 'releases/mozilla-release',
-            'product': 'firefox',
-            'version': '51.0.1',
-        }
-
-        self.release_object_54_0b1 = {
+    def test_beta_1_release(self):
+        release_object_54_0b1 = {
             'branch': 'releases/mozilla-beta',
             'product': 'firefox',
             'version': '54.0b1',
         }
+        buglist_str_54_0b1 = create_bugs_url(release_object_54_0b1)
+        assert buglist_str_54_0b1 == '', 'There should be no bugs to compare for beta 1.'
 
-        self.release_object_53_0b10 = {
-            'branch': 'releases/mozilla-beta',
-            'product': 'firefox',
-            'version': '53.0b10',
+    def test_is_test_only_change(self):
+        test_only_description = {
+            'desc': 'something something something a=test-only something something something',
         }
+        assert is_test_only_change(test_only_description)
 
-    def test_51_0_1(self):
-        buglist_str = create_bugs_url(self.release_object_51_0_1)
-        desc, regular, backouts = buglist_str.split('\n')
+    def test_is_backout_bug(self):
+        backout_bugs_descs = [
+            'I backed out this bug because',
+            'Backing out this bug due to',
+            'Backout bug xyz',
+            'Back out bug xyz',
+        ]
+        for backout_desc in backout_bugs_descs:
+            assert is_backout_bug(backout_desc.lower()), 'is_backout_bug did not catch a backout.'
 
-        assert desc == 'Comparing Mercurial tag FIREFOX_51_0_RELEASE to FIREFOX_51_0_1_RELEASE:', 'Mercurial tags improperly generated'
-        assert all(bug in regular for bug in ['1294650', '1333516', '1331808', '1333423', '1333663']), 'Some bugs are missing from the string'
-        assert backouts == '', 'There should be no backouts for 51.0.1'
+    def test_get_tag_versions(self):
+        current_and_previous_tag_versions = [
+            (['firefox', 'releases/mozilla-beta', '53.0b10'], ('FIREFOX_53_0b10_RELEASE', 'FIREFOX_53_0b9_RELEASE')),
+            (['firefox', 'releases/mozilla-release', '51.0.1'], ('FIREFOX_51_0_1_RELEASE', 'FIREFOX_51_0_RELEASE')),
+            (['fennec', 'releases/mozilla-beta', '52.0b4'], ('FENNEC_52_0b4_RELEASE', 'FENNEC_52_0b2_RELEASE')),
+        ]
+        for args, expected in current_and_previous_tag_versions:
+            results = get_tag_versions(args[0], args[1], args[2])
+            assert expected == results
 
-    def test_beta_54_0b1(self):
-        buglist_str = create_bugs_url(self.release_object_54_0b1)
+    def test_format_return_value(self):
+        desc = 'Went from release a to release b\n'
+        new_bugs = {'1220832', '1220837', '1347119'}
+        backout_bugs = {'1340639', '1340641'}
+        formatted_return_value = format_return_value(desc, new_bugs, backout_bugs)
 
-        assert buglist_str == '', 'Beta 1 releases should have no bugs to display.'
+        lines = formatted_return_value.split('\n')
 
-    def test_beta_53_0b10(self):
-        buglist_str = create_bugs_url(self.release_object_53_0b10)
-        desc, regular, backouts = buglist_str.split('\n')[:-1]
+        assert not formatted_return_value.endswith('\n'), "There shouldn't be a newline at the end."
+        assert lines[0] == desc[:-1], 'Description in output does not match description in input.'
+        assert lines[1] and lines[2], 'Incorrect formatting of the second and third lines.'
 
-        assert desc == 'Comparing Mercurial tag FIREFOX_53_0b9_RELEASE to FIREFOX_53_0b10_RELEASE:', 'Mercurial tags improperly formatted, check 9 is before 10'
-        assert all(str(bug) in regular
-                   for bug in [1350661, 1350783, 1352926, 1350844, 1345873, 1353129, 1317191, 1333486, 1353476, 1344467,
-                               1352406, 1341352, 1349581, 1353439, 1347075, 1352367, 1314543, 1328001, 1348941, 1349862,
-                               1345687, 1342433, 1353740, 1352373, 1353333, 1348454, 1351964, 1353373, 1354496, 1333858,
-                               1347617, 1349650, 1353041, 1354115, 1349719, 1346862, 1349612, 1353789]), 'Some bugs are missing'
-        assert all(str(bug) in backouts
-                   for bug in [1346862, 1352406, 1349719, 1192818]), 'Some backout bugs are missing'
+    def test_create_short_url_with_prefix(self):
+        new_bugs = {'1220832', '1220837', '1347119'}
+        backout_bugs = {'1340639', '1340641'}
+        regular_bug_link, backout_bug_link = create_short_url_with_prefix(new_bugs, backout_bugs)
 
-    # TODO: Test ESR releases, test-only changes excluded,
+        assert 'Bugs since previous changeset' in regular_bug_link
+        assert 'Backouts since previous changeset' in backout_bug_link
 
+    def test_get_bugs_in_changeset(self):
+        url = 'https://hg.mozilla.org/releases/mozilla-release/json-pushes?fromchange=d345b657d381&tochange=f87a819106bd&full=1'
+        changeset_data = requests.get(url).json()
+        bugs, backouts = get_bugs_in_changeset(changeset_data)
 
-class TestCreateBugsUrlMobile(unittest.TestCase):
-
-    def setUp(self):
-        self.release_object_53_0b1 = {
-            'branch': 'releases/mozilla-release',
-            'version': '53.0b1',
-            'product': 'fennec'
-        }
-
-        self.release_object_38_0b10 = {
-            'branch': 'releases/mozilla-release',
-            'version': '38.0b10',
-            'product': 'fennec',
-        }
-
-        self.release_object_xx_xx = {
-            'branch': '',
-            'version': '',
-            'product': 'fennec'
-        }
-
-        self.release_object_xx_xx = {
-            'branch': '',
-            'version': '',
-            'product': 'fennec'
-        }
-
-        self.release_object_xx_xx = {
-            'branch': '',
-            'version': '',
-            'product': 'fennec'
-        }
-
-        self.release_object_xx_xx = {
-            'branch': '',
-            'version': '',
-            'product': 'fennec'
-        }
-
-    def test_53_0b1(self):
-        buglist_str = create_bugs_url(self.release_object_53_0b1)
-        assert buglist_str == '', 'There should be no bugs to compare for beta 1.'
-
-    def test_53_0b10(self):
-        buglist_str = create_bugs_url(self.release_object_38_0b10)
+        assert bugs == {'1356563', '1344529', '1348409', '1341190', '1360626', '1337861', '1332731', '1328762',
+                        '1306543', '1355870', '1358089', '1354911', '1354038'}
+        assert backouts == {'1337861', '1320072'}
 
 
+class TestHgJSONAPI(unittest.TestCase):
+    """Tests the Hg JSON API to ensure correct behaviour"""
+
+    def test_tags_json_format(self):
+        json_tags_request_string = 'https://hg.mozilla.org/releases/mozilla-beta/json-tags'
+        tags_json = requests.get(json_tags_request_string).json()
+        assert 'tags' in tags_json, 'tags field missing from json-tags API.'
+
+        for tag in tags_json['tags']:
+            assert 'tag' in tag, 'Hg JSON API missing tag subfield.'
+
+    def test_changeset_json_format(self):
+        json_changeset_request_string = 'https://hg.mozilla.org/releases/mozilla-release/json-pushes?fromchange=FIREFOX_51_0_RELEASE&tochange=FIREFOX_51_0_1_RELEASE&full=1'
+        changeset_json = requests.get(json_changeset_request_string).json()
+        for changeset_entry in changeset_json.values():
+            assert 'changesets' in changeset_entry, 'No changeset field for {}'.format(changeset_entry)
+
+            for changeset in changeset_entry['changesets']:
+                assert 'desc' in changeset
